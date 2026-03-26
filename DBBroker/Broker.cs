255fcf4eb4489dade8ad7466abd268ca42cca8ec
perab <perab@PEKIROG>
2026-03-26 11:22:38 +0100

    Add project files.

---
+++ DBBroker/Broker.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Data.SqlClient;
+using Zajednicki.Domen;
+
+namespace DBBroker
+{
+    public class Broker
+    {
+        private DbConnection connection;
+
+        public Broker()
+        {
+            connection = new DbConnection();
+        }
+
+        public void Rollback()
+        {
+            connection.RallBack();
+        }
+
+        public void Commit()
+        {
+            connection.Commit();
+        }
+
+        public void BeginTransaction()
+        {
+            connection.BeginTransaction();
+        }
+        public void CloseConnection()
+        {
+            connection.CloseConnection();
+        }
+
+        public void OpenConnection()
+        {
+            connection.OpenConnection();
+        }
+
+        public List<IEntity> GetAll(IEntity entity)
+        {
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = $"select * from {entity.TablName}";
+            using SqlDataReader reader = command.ExecuteReader() ;
+            List<IEntity> list = entity.GetReaderList(reader) ;
+            return list ;
+        }
+
+        public void Add(IEntity entity)
+        {
+            SqlCommand cmd = connection.CreateCommand();
+            cmd.CommandText = $"insert into {entity.TablName} values ({entity.Values})";
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+        }
+    }
+}
---
+++ Zajednicki/Domen/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Zajednicki.Domen
+{
+    public interface IEntity
+    {
+        string TablName { get; }
+
+        string Values { get; }
+
+        List<IEntity> GetReaderList(SqlDataReader reader);
+    }
+}
